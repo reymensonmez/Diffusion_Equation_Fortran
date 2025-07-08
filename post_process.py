37e5d5754c77d93dd55e2e9e934916bbf6612ca7
Rabia Sonmez <sonmez.eymenrabia@gmail.com>
2025-07-08 15:41:42 -0400

    Delete post_process.py

--- post_process.py
+++
@@ -1,1 +0,0 @@
-import numpy as npimport matplotlib.pyplot as pltdata = np.loadtxt('solution.dat')n = int(np.sqrt(data.shape[0]))Z = data[:,2].reshape((n,n))x = np.linspace(0, 1, n)y = np.linspace(1, 0, n)X, Y = np.meshgrid(x, y)plt.contourf(X, Y, Z, levels=12)plt.colorbar()plt.title("2D Diffusion Equation")plt.show()
